992dd62a50eab340a9c52de4713e67b8d3aa0687
Alejandro Martín Millarengo <alexmarmill@gmail.com>
2024-10-28 17:01:34 +0100

    Exercise12

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -2,4 +2,83 @@ package com.campusdual.classroom;
 
 public class Car {
 
+    public final int MAX_SPEED = 200;
+    public final int MAX_ANGLE = 45;
+
+
+
+    int tachometer = 0; // Tacómetro inicial en 0
+    int speedometer = 0; // Velocímetro inicial en 0
+    int wheelsAngle = 0; // Ángulo de las ruedas inicial en 0
+    boolean isOn = false; // Estado del coche apagado
+    String gear = "N"; // Marcha inicial en neutral
+
+
+
+
+    public boolean isTachometerGreaterThanZero() {
+        return tachometer > 0;
+    }
+
+
+    public boolean isTachometerEqualToZero() {
+        return tachometer == 0;
+    }
+
+
+    public void start() {
+        if (!isOn) {
+            isOn = true;
+            tachometer = 700;
+        }
+    }
+
+
+    public void stop() {
+        if (speedometer == 0) {
+            isOn = false;
+            tachometer = 0;
+        }
+    }
+
+
+    public void accelerate() {
+        if (isOn && speedometer < MAX_SPEED) {
+            speedometer += 10;
+            if (speedometer > MAX_SPEED) {
+                speedometer = MAX_SPEED;
+            }
+        }
+    }
+
+
+    public void brake() {
+        if (speedometer > 0) {
+            speedometer -= 10;
+            if (speedometer < 0) {
+                speedometer = 0;
+            }
+        }
+    }
+
+
+    public void turnAngleOfWheels(int angle) {
+        int newAngle = wheelsAngle + angle;
+        if (newAngle > MAX_ANGLE) {
+            wheelsAngle = MAX_ANGLE;
+        } else if (newAngle < -MAX_ANGLE) {
+            wheelsAngle = -MAX_ANGLE;
+        } else {
+            wheelsAngle = newAngle;
+        }
+    }
+
+
+    public void setReverse(boolean reverse) {
+        if (reverse && speedometer == 0) {
+            gear = "R";
+        } else if (!reverse) {
+            gear = "N";
+        }
+    }
 }
--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -3,6 +3,61 @@ package com.campusdual.classroom;
 public class Exercise12 {
     public static void main(String[] args) {
 
-    }
+        Car myCar = new Car();
 
+
+        System.out.println("Tacómetro es cero: " + myCar.isTachometerEqualToZero());
+
+
+        myCar.start();
+        System.out.println("Coche encendido, tacómetro > 0: " + myCar.isTachometerGreaterThanZero());
+
+
+        myCar.start();
+        System.out.println("Intento de encendido mientras está encendido: " + (myCar.tachometer > 0));
+
+
+        myCar.stop();
+        System.out.println("Coche apagado, tacómetro es cero: " + myCar.isTachometerEqualToZero());
+
+
+        myCar.start();
+        myCar.accelerate();
+        System.out.println("Velocidad después de acelerar: " + myCar.speedometer);
+
+
+        myCar.stop();
+        System.out.println("Intento de apagar en movimiento, tacómetro > 0: " + (myCar.tachometer > 0));
+
+
+        for (int i = 0; i < 30; i++) {
+            myCar.accelerate();
+        }
+        System.out.println("Velocidad máxima alcanzada: " + myCar.speedometer);
+
+
+        for (int i = 0; i < 30; i++) {
+            myCar.brake();
+        }
+        System.out.println("Velocidad después de frenar hasta detenerse: " + myCar.speedometer);
+
+
+        myCar.turnAngleOfWheels(20);
+        System.out.println("Ángulo de las ruedas tras girar 20 grados: " + myCar.wheelsAngle);
+
+
+        myCar.turnAngleOfWheels(50);
+        System.out.println("Ángulo de las ruedas tras intentar girar 50 grados: " + myCar.wheelsAngle);
+
+
+        myCar.brake(); // Asegurarse de que la velocidad sea cero
+        myCar.setReverse(true);
+        System.out.println("Marcha trasera activada (R): " + myCar.gear);
+
+
+        myCar.start();
+        myCar.accelerate();
+        myCar.setReverse(true);
+        System.out.println("Intento de marcha atrás en movimiento, marcha actual: " + myCar.gear);
+    }
 }
